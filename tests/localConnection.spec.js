c67f89fd8e3179cf2c900c3e6667c498dfb8f891
Nicolas Galler <ngaller@gmail.com>
2017-07-21 06:46:05 -0400

    enhance get method to accept an ObjectID

--- tests/localConnection.spec.js
+++ tests/localConnection.spec.js
@@ -13,104 +13,116 @@ const customerConfig = {
 }
 // localConnection is responsible for abstracting the mongo database
 describe('localConnection', () => {
-  it('has a customers collection', async () => {
-    const db = await testConnection()
-    const lc = new LocalConnection(db)
+  let db, lc
+
+  beforeEach(async () => {
+    db = await testConnection()
+    lc = new LocalConnection(db)
     lc.addCollection(customerConfig)
+    await db.collection('Customers').remove({})
+  })
+
+  it('has a customers collection', async () => {
     expect(lc.Customers).to.be.ok
     lc.Customers.should.have.property('upsert').that.is.a('function')
   })
 
-  it('inserts a new record', async () => {
-    const db = await testConnection()
-    const lc = new LocalConnection(db)
-    lc.addCollection(customerConfig)
-    return lc.Customers.upsert({CustNum: '6'}, 'CustNum').then(upsertResult => {
-      expect(upsertResult.updated).to.equal(0)
-      expect(upsertResult.inserted).to.equal(1)
-      return db.collection('Customers').findOne({CustNum: '6'}).then((result) => {
-        expect(result.CustNum).to.equal('6')
-        expect(String(result._id)).to.equal(String(upsertResult.recordId))
+  describe('upsert', () => {
+    it('inserts a new record', async () => {
+      return lc.Customers.upsert({CustNum: '6'}, 'CustNum').then(upsertResult => {
+        expect(upsertResult.updated).to.equal(0)
+        expect(upsertResult.inserted).to.equal(1)
+        return db.collection('Customers').findOne({CustNum: '6'}).then((result) => {
+          expect(result.CustNum).to.equal('6')
+          expect(String(result._id)).to.equal(String(upsertResult.recordId))
+        })
       })
+    })
+
+    it('updates existing record', async () => {
+      return db.collection('Customers').insert({CustNum: '10', Name: 'Poudroux', Amount: 24}).then(() =>
+        lc.Customers.upsert({CustNum: '10', Name: 'Testing', OtherField: 123}).then(upsertResult => {
+          expect(upsertResult.updated).to.equal(1)
+          expect(upsertResult.inserted).to.equal(0)
+          return db.collection('Customers').findOne({CustNum: '10'}).then((result) => {
+            expect(omit(result, '_id')).to.eql({
+              CustNum: '10', Name: 'Testing', Amount: 24, OtherField: 123
+            })
+            expect(String(result._id)).to.equal(String(upsertResult.recordId))
+          })
+        }))
     })
   })
 
-  it('updates existing record', async () => {
-    const db = await testConnection()
-    const lc = new LocalConnection(db)
-    lc.addCollection(customerConfig)
-    return db.collection('Customers').insert({CustNum: '10', Name: 'Poudroux', Amount: 24}).then(() =>
-      lc.Customers.upsert({CustNum: '10', Name: 'Testing', OtherField: 123}).then(upsertResult => {
-        expect(upsertResult.updated).to.equal(1)
-        expect(upsertResult.inserted).to.equal(0)
-        return db.collection('Customers').findOne({CustNum: '10'}).then((result) => {
-          expect(omit(result, '_id')).to.eql({
-            CustNum: '10', Name: 'Testing', Amount: 24, OtherField: 123
-          })
-          expect(String(result._id)).to.equal(String(upsertResult.recordId))
-        })
-      }))
+  describe('update', () => {
+    it('updates using identifier', async () => {
+      const r = await  db.collection('Customers').insertOne({CustNum: 'WITH-ID', Name: 'Poudroux', Amount: 24})
+      const id = r.insertedId
+      await lc.Customers.update({Name: 'Joe'}, id)
+      const modified = await db.collection('Customers').findOne({_id: id})
+      expect(modified).to.be.ok
+      expect(modified.Name).to.equal('Joe')
+    })
+
+    it('updates multiple records', async () => {
+      await lc.Customers.upsert({CustNum: '10', ParentId: 'the-parent'})
+      await lc.Customers.upsert({CustNum: '11', ParentId: 'the-parent'})
+      await lc.Customers.update({ParentName: 'The name'}, {ParentId: 'the-parent'})
+      for(let CustNum of ['10', '11']) {
+        let customer = await db.collection('Customers').findOne({CustNum})
+        expect(customer, CustNum).to.have.property('ParentName').that.eql('The name')
+      }
+    })
   })
 
-  it('updates using identifier', async () => {
-    const db = await testConnection()
-    const lc = new LocalConnection(db)
-    lc.addCollection(customerConfig)
-    const r = await  db.collection('Customers').insertOne({CustNum: 'WITH-ID', Name: 'Poudroux', Amount: 24})
-    const id = r.insertedId
-    await lc.Customers.update({Name: 'Joe'}, id)
-    const modified = await db.collection('Customers').findOne({_id: id})
-    expect(modified).to.be.ok
-    expect(modified.Name).to.equal('Joe')
+  describe('addOrUpdateChildInCollection', () => {
+    it('adds child to collection', async () => {
+      await db.collection('Customers').remove({})
+      await lc.Customers.upsert({CustNum: '10', ParentId: 'the-parent', Children: [{ChildId: '1'}]})
+      await lc.Customers.addOrUpdateChildInCollection('10', 'Children', {ChildId: '2', Content: 'Test'}, 'ChildId')
+      const customer = await lc.Customers.get({CustNum: '10'})
+      expect(customer.Children).to.eql([{ChildId: '1'}, {ChildId: '2', Content: 'Test'}])
+    })
+
+    it('adds child to collection when it does not exist', async () => {
+      await db.collection('Customers').remove({})
+      await lc.Customers.upsert({CustNum: '10', ParentId: 'the-parent'})
+      await lc.Customers.addOrUpdateChildInCollection('10', 'Children', {ChildId: '2', Content: 'Test'}, 'ChildId')
+      const customer = await lc.Customers.get({CustNum: '10'})
+      expect(customer.Children).to.eql([{ChildId: '2', Content: 'Test'}])
+    })
+
+    it('replaces existing child in collection', async () => {
+      await db.collection('Customers').remove({})
+      await lc.Customers.upsert({CustNum: '10', ParentId: 'the-parent', Children: [{ChildId: '1'}]})
+      await lc.Customers.addOrUpdateChildInCollection('10', 'Children', {ChildId: '1', Content: 'Test'}, 'ChildId')
+      const customer = await lc.Customers.get({CustNum: '10'})
+      expect(customer.Children).to.eql([{ChildId: '1', Content: 'Test'}])
+    })
   })
 
-  it('updates multiple records', async () => {
-    const db = await testConnection()
-    const lc = new LocalConnection(db)
-    lc.addCollection(customerConfig)
-    await lc.Customers.upsert({CustNum: '10', ParentId: 'the-parent'})
-    await lc.Customers.upsert({CustNum: '11', ParentId: 'the-parent'})
-    await lc.Customers.update({ParentName: 'The name'}, {ParentId: 'the-parent'})
-    for(let CustNum of ['10', '11']) {
-      let customer = await db.collection('Customers').findOne({CustNum})
-      expect(customer, CustNum).to.have.property('ParentName').that.eql('The name')
-    }
+  describe('removeChildFromCollection', () => {
+    it.skip('removes child from collection', async () => {
+    })
   })
 
-  it('adds child to collection', async () => {
-    const db = await testConnection()
-    const lc = new LocalConnection(db)
-    lc.addCollection(customerConfig)
-    await db.collection('Customers').remove({})
-    await lc.Customers.upsert({CustNum: '10', ParentId: 'the-parent', Children: [{ChildId: '1'}]})
-    await lc.Customers.addOrUpdateChildInCollection('10', 'Children', {ChildId: '2', Content: 'Test'}, 'ChildId')
-    const customer = await lc.Customers.get({CustNum: '10'})
-    expect(customer.Children).to.eql([{ChildId: '1'}, {ChildId: '2', Content: 'Test'}])
-  })
+  describe('get', () => {
+    let customerId
 
-  it('adds child to collection when it does not exist', async () => {
-    const db = await testConnection()
-    const lc = new LocalConnection(db)
-    lc.addCollection(customerConfig)
-    await db.collection('Customers').remove({})
-    await lc.Customers.upsert({CustNum: '10', ParentId: 'the-parent'})
-    await lc.Customers.addOrUpdateChildInCollection('10', 'Children', {ChildId: '2', Content: 'Test'}, 'ChildId')
-    const customer = await lc.Customers.get({CustNum: '10'})
-    expect(customer.Children).to.eql([{ChildId: '2', Content: 'Test'}])
-  })
+    beforeEach(async () => {
+      const customer = await lc.Customers.upsert({CustNum: '10', ParentId: 'the-parent', Children: [{ChildId: '1'}]})
+      customerId = customer.recordId
+    })
 
-  it('replaces existing child in collection', async () => {
-    const db = await testConnection()
-    const lc = new LocalConnection(db)
-    lc.addCollection(customerConfig)
-    await db.collection('Customers').remove({})
-    await lc.Customers.upsert({CustNum: '10', ParentId: 'the-parent', Children: [{ChildId: '1'}]})
-    await lc.Customers.addOrUpdateChildInCollection('10', 'Children', {ChildId: '1', Content: 'Test'}, 'ChildId')
-    const customer = await lc.Customers.get({CustNum: '10'})
-    expect(customer.Children).to.eql([{ChildId: '1', Content: 'Test'}])
-  })
+    it('gets a record using a selector', async () => {
+      const cust = await lc.Customers.get({CustNum: '10'})
+      expect(cust).to.be.ok.and.to.have.property('ParentId').that.equals('the-parent')
+    })
 
-  it.skip('removes child from collection', async () => {
+    it('gets a record using a Mongo ID', async () => {
+      const cust = await lc.Customers.get(customerId)
+      expect(cust).to.be.ok.and.to.have.property('ParentId').that.equals('the-parent')
+    })
   })
 
   // it('updates record using composite key', async () => {
